b914f3d9ab127a20e8fbdc7b36521ec31c85cdc9
Yixin Luo <18810541851@163.com>
2016-06-11 16:15:58 +0800

    检测系统支持的io复用类型

---
+++ core/sys_compat.sh
@@ -0,0 +1,29 @@
+cat > .test.h <<'EOM'
+#include <sys/epoll.h>
+EOM
+if gcc -E .test.h
+ then
+  echo '#define HAVE_EPOLL_H 1' >> config.h
+ else 
+  echo '#ifdef HAVE_EPOLL_H' >> config.h
+  echo '# undef HAVE_EPOLL_H' >> config.h
+  echo '#endif' >> config.h
+ fi
+
+rm .test.h
+
+cat > .test.h <<'EOM'
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+int a = kqueue();
+}
+EOM
+if gcc -E .test.h
+ then
+  echo '#define HAVE_KQUEUE_H 1' >> config.h
+ else 
+  echo '#ifdef HAVE_KQUEUE_H' >> config.h
+  echo '# undef HAVE_KQUEUE_H' >> config.h
+  echo '#endif' >> config.h
+ fi
